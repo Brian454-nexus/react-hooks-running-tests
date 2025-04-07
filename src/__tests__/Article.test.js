275274f77146a4a441aa1d5df59359c5c601c01c
Brian454-nexus <brian.terer@student.moringaschool.com>
2025-04-07 23:25:35 +0300

    Added hooks for running tests

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -6,6 +6,7 @@ import Article from "../components/Article";
 
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
-
+  // screen.debug(); // Uncomment this line to see the rendered output in the console
+  
   expect(screen.queryByText("please pass this test")).toBeInTheDocument();
 });
